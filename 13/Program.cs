61230e35a2f80dcee70427680a763f917754a7fc
dmitriy <master71512@mail.ru>
2022-09-23 23:22:23 +0300

    13 solved

---
+++ 13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100)
+{
+    Console.WriteLine("3-й цифры нет");
+}
+else
+{
+    int num3 = -1;
+    while (num >= 100)
+    {
+        num3 = num % 10;
+        num = num / 10;
+    }
+    Console.WriteLine(num3);
+}
