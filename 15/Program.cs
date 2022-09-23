092847a7c66ddae61b841797e19a5449471d68b9
dmitriy <master71512@mail.ru>
2022-09-23 23:36:51 +0300

    15 solved

---
+++ 15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num < 1) | (num > 7))
+{
+    Console.WriteLine("некорректный номер");
+}
+else
+{
+    if ((num == 6) | (num == 7))
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
