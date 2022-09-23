76a25f132f13eae5bde38c8ff64799e6347caa0d
dmitriy <master71512@mail.ru>
2022-09-23 22:04:19 +0300

    10 solved

---
+++ 10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите 3-значное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num <100) | (num > 999))
+{
+    Console.WriteLine("Число не 3-значное");
+}
+else
+{
+    int num2 = num / 10 % 10;
+    Console.WriteLine(num2);
+}
